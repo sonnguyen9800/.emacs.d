da8e78594aa3abb9d58e730784156f9909e78904
sonnguyen9800 <sonnguyen9800@gmail.com>
2020-03-13 16:32:03 +0700

    More packages added to init

--- config/init-package.el
+++ config/init-package.el
@@ -1,8 +1,8 @@
 ;;Package to be installed:
 (setq package-list '(magit yasnippet web-mode company-tern js2-mode js2-refactor xref-js2
 			   helm neotree magit beacon solarized-theme centaur-tabs projectile
-
 			   php-mode company-php
+			   lsp-mode company-lsp flycheck
 			   google-this
 			   emmet-mode web-beautify auto-complete highlight-parentheses zoom))
 (package-initialize)
